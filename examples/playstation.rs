89ef5cf1ab483c5dfe1a94ee09d5739f5c87cd58
Edwin Amsler <edwinguy@gmail.com>
2018-04-08 14:14:05 -0500

    Lowered bitrate because my test rig is electrically noisey, increased output

--- examples/playstation.rs
+++ examples/playstation.rs
@@ -12,7 +12,7 @@ fn build_spi() -> io::Result<Spidev> {
 	let mut spi = Spidev::open("/dev/spidev32766.0")?;
 	let opts = SpidevOptions::new()
 		.bits_per_word(8)
-		.max_speed_hz(50_000)
+		.max_speed_hz(20_000)
 		.mode(SPI_MODE_3)
 		.build();
 	spi.configure(&opts)?;
@@ -24,20 +24,27 @@ fn main() {
 	let spi = build_spi().unwrap();
 	let mut psp = PlayStationPort::new(spi);
 
+    psp.enable_pressure();
+
     loop {
         let controller = psp.read_buttons();
 
         match controller {
-            Device::None => println!("Missing"),
+            Device::None => {
+                println!("Missing.");
+            },
             Device::Classic(x) => {
-                println!("Start? {0}", 
-                    x.buttons.start());
+                println!("Start? {0}, Square? {1}",
+                    x.buttons.start(),
+                    x.buttons.square());
             },
             Device::DualShock(x) => {
-                println!("Start? {0} - R:{1:02x},{2:02x}", 
+                println!("Start? {0} - R:{1:02x},{2:02x}, L:{3:02x},{4:02x}", 
                     x.buttons.start(),
                     x.rx,
-                    x.ry);
+                    x.ry,
+                    x.lx,
+                    x.ly);
             },
             Device::DualShock2(x) => {
                 println!("Start? {0} - R:{1:02x},{2:02x} - X Pressure:{3:02x}", 
