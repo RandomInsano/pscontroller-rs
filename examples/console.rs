01ae0857cb6941fc76e4da806599a7ec17bfcb29
Edwin Amsler <edwinguy@gmail.com>
2018-04-22 14:35:47 -0500

    Get 'console' example actually using the library. :D

--- examples/console.rs
+++ examples/console.rs
@@ -2,20 +2,28 @@
 
 extern crate linux_embedded_hal as linux_hal;
 extern crate embedded_hal;
-extern crate bit_reverse;
+extern crate pscontroller_rs;
 
-use bit_reverse::ParallelReverse;
 use std::io;
 use std::{thread, time};
-use linux_hal::spidev::{Spidev, SpidevOptions, SpidevTransfer, SPI_MODE_3};
+use linux_hal::Spidev;
+use linux_hal::spidev::{SpidevOptions, SPI_MODE_3};
+use linux_hal::Pin;
+
+use pscontroller_rs::PlayStationPort;
+
+// Specific to the host device used on Linux, you'll have to change the following
+// parameters depending on your board and also export and allow writing to the GPIO
+const SPI_ENABLE_PIN: u64 = 1020; // XIO-7 on the NTC CHIP
+const SPI_DEVICE: &str = "/dev/spidev32766.0"; // Needs a device-tree overlay
+const SPI_SPEED: u32 = 10_000; // Due to a bug, 50KHz is the best the CHIP can do semi-reliably
 
 fn build_spi() -> io::Result<Spidev> {
-	let mut spi = Spidev::open("/dev/spidev32766.0")?;
+	let mut spi = Spidev::open(SPI_DEVICE)?;
 	let opts = SpidevOptions::new()
 		.bits_per_word(8)
-		.max_speed_hz(50_000)
+		.max_speed_hz(SPI_SPEED)
 		.mode(SPI_MODE_3)
-		//.lsb_first(true)  // Hardware may not support this
 		.build();
 	spi.configure(&opts)?;
 
@@ -23,17 +31,15 @@ fn build_spi() -> io::Result<Spidev> {
 }
 
 fn main() {
-	let spi = build_spi().unwrap();
+    let spi = build_spi().unwrap();
+    let enable_pin = Pin::new(SPI_ENABLE_PIN);    
+    let mut psp = PlayStationPort::new(spi, enable_pin);
 	let mut command = [0u8; 21];
 	let mut buffer = [0u8; 21];
 
 	command[0] = 0x01;
 	command[1] = 0x42;
 	command[2] = 0x00;
-
-	for item in command.iter_mut() {
-		*item = item.swap_bits();
-	}
 
 	let mut now = time::Instant::now();
 	let sleep_duration = time::Duration::from_micros(30_000);
@@ -45,10 +51,8 @@ fn main() {
 	loop {
 		thread::sleep(sleep_duration);	
 
-		{
-			let mut transfer = SpidevTransfer::read_write(&command, &mut buffer);
-			spi.transfer(&mut transfer).unwrap();
-		}
+		psp.send_command(&command, &mut buffer).unwrap()
+		;
 
 		if now.elapsed() > sample_duration {
 			now = time::Instant::now();
@@ -60,7 +64,7 @@ fn main() {
 		print!("Rate: ({}) - ", rate);
 
 		for item in buffer.iter() {
-			print!("{:02x} ", item.swap_bits());
+			print!("{:02x} ", item);
 		}
 
 		if buffer[1] == 0xff {
