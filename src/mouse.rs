5d73f89d0a39759c156c4a5f0c68c2e8efa18bd8
Edwin Amsler <edwinguy@gmail.com>
2018-09-03 20:40:35 +0000

    Actually add the source code

---
+++ src/mouse.rs
@@ -0,0 +1,38 @@
+//! PlayStation Mouse
+//! ============================
+//! That [standard looking mouse](https://en.wikipedia.org/wiki/PlayStation_Mouse)
+//! for the PlayStation. This was implemented from notes online and while it
+//! should be accurate, it has not been tested.
+
+/// The two buttons found on the mouse
+#[repr(C)]
+#[derive(Clone)]
+pub struct MouseButtons {
+    data: u16
+}
+
+impl MouseButtons {
+    const PM_L: u16 = 0x0400;
+    const PM_R: u16 = 0x0800;
+
+    /// A button on the controller
+    pub fn left(&self) -> bool {
+        self.data & Self::PM_L == 0
+    }
+
+    /// A button on the controller
+    pub fn right(&self) -> bool {
+        self.data & Self::PM_R == 0
+    }
+}
+
+#[repr(C)]
+/// Represents the classic Controller
+pub struct Mouse {
+    /// Standard buttons
+    pub buttons: MouseButtons,
+    /// Difference in Y-Axis since last poll
+    pub y: i8,
+    /// Difference in X-Axis since last poll
+    pub x: i8,
+}
