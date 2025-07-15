2b2ddaddca3b8c4d04d452ddd7a6d469f17f1538
Rex Magana <Rex.magana@gmail.com>
2025-07-15 11:08:13 -0700

    non anonymous args

--- src/lib.rs
+++ src/lib.rs
@@ -204,7 +204,7 @@ pub trait PollCommand {
     /// Re-write the provided slice starting from index 0. This command
     /// is called by read_input() which will provide a sub-slice of the
     /// controller's command bytes.
-    fn set_command(&self, &mut [u8]);
+    fn set_command(&self, _: &mut [u8]);
 }
 
 /// Many controllers have the same set of buttons (Square, Circle, L3, R1, etc).
@@ -416,7 +416,7 @@ where
         Ok(config)
     }
 
-    fn read_port(&mut self, command: Option<&PollCommand>) -> Result<[u8; MESSAGE_MAX_LENGTH], Error<E>> {
+    fn read_port(&mut self, command: Option<&dyn PollCommand>) -> Result<[u8; MESSAGE_MAX_LENGTH], Error<E>> {
         let mut buffer = [0u8; MESSAGE_MAX_LENGTH];
         let mut data = [0u8; MESSAGE_MAX_LENGTH];
 
@@ -448,7 +448,7 @@ where
     /// Get the raw data from polling for a controller. You can use this to cooerce the data into
     /// some controller that can't be safely identified by `read_input`, but you should rely on that
     /// function if you can.
-    pub fn read_raw(&mut self, command: Option<&PollCommand>) -> Result<ControllerData, Error<E>> {
+    pub fn read_raw(&mut self, command: Option<&dyn PollCommand>) -> Result<ControllerData, Error<E>> {
         let mut buffer = [0u8; MESSAGE_MAX_LENGTH];
         let data = self.read_port(command)?;
 
@@ -463,7 +463,7 @@ where
 
     /// Ask the controller for input states. Different contoller types will be returned automatically
     /// for you. If you'd like to cooerce a controller yourself, use `read_raw`.
-    pub fn read_input(&mut self, command: Option<&PollCommand>) -> Result<Device, Error<E>> {
+    pub fn read_input(&mut self, command: Option<&dyn PollCommand>) -> Result<Device, Error<E>> {
         let mut buffer = [0u8; MESSAGE_MAX_LENGTH];
         let data = self.read_port(command)?;
 
